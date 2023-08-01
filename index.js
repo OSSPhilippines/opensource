1e65c04b35b841bff7b2866976fa0648f01f60f0
Joff Tiquez <jofftiquez@gmail.com>
2023-08-01 11:42:01 +0800

    aider: Revert changes to index.js.
    # Aider chat conversation:
    
    USER: revert changes to ind
    
    ASSISTANT: I'm sorry, but your request is a bit unclear. Could
    ^C KeyboardInterrupt
    
    USER: revert changes to index.js
    
    ASSISTANT: To revert the changes to the `index.js` file, we need to replace the current content with the original content. Here is the code modification for the `index.js` file:
    
    ```javascript
    index.js
    <<<<<<< ORIGINAL
    const fs = require('fs');
    
    // Read the package.json file
    const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
    
    // Generate the README.md content
    const readmeContent = `# ${packageJson.name}\n\n${packageJson.description}\n\n## Author\n\n${packageJson.author}\n\n## License\n\n${packageJson.license}`;
    
    // Write the README.md file
    fs.writeFileSync('README.md', readmeContent);
    =======
    const fs = require('fs');
    const readline = require('readline');
    
    const rl = readline.createInterface({
      input: process.stdin,
      output: process.stdout
    });
    
    rl.question('What is your project name? ', (name) => {
      rl.question('What is your project description? ', (description) => {
        rl.question('What is your project license? ', (license) => {
          rl.question('What is the author name for package.json? ', (authorName) => {
            fs.mkdirSync(name);
    
            const packageJsonContent = {
              name,
              description,
              license,
              author: authorName
            };
    
            fs.writeFileSync(`${name}/package.json`, JSON.stringify(packageJsonContent, null, 2));
            fs.writeFileSync(`${name}/LICENSE`, license);
            fs.writeFileSync(`${name}/README.md`, `# ${name}\n\n${description}`);
            fs.writeFileSync(`${name}/CONTRIBUTING.md`, `# Contributing to ${name}\n\n`);
            fs.writeFileSync(`${name}/CODE_OF_CONDUCT.md`, `# Code of Conduct\n\n`);
    
            rl.close();
          });
        });
      });
    });
    >>>>>>> UPDATED
    ```
    
    This modification will revert the `index.js` file to its original state.

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const fs = require('fs');
+const readline = require('readline');
 
-// Read the package.json file
-const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-// Generate the README.md content
-const readmeContent = `# ${packageJson.name}\n\n${packageJson.description}\n\n## Author\n\n${packageJson.author}\n\n## License\n\n${packageJson.license}`;
+rl.question('What is your project name? ', (name) => {
+  rl.question('What is your project description? ', (description) => {
+    rl.question('What is your project license? ', (license) => {
+      rl.question('What is the author name for package.json? ', (authorName) => {
+        fs.mkdirSync(name);
 
-// Write the README.md file
-fs.writeFileSync('README.md', readmeContent);
+        const packageJsonContent = {
+          name,
+          description,
+          license,
+          author: authorName
+        };
+
+        fs.writeFileSync(`${name}/package.json`, JSON.stringify(packageJsonContent, null, 2));
+        fs.writeFileSync(`${name}/LICENSE`, license);
+        fs.writeFileSync(`${name}/README.md`, `# ${name}\n\n${description}`);
+        fs.writeFileSync(`${name}/CONTRIBUTING.md`, `# Contributing to ${name}\n\n`);
+        fs.writeFileSync(`${name}/CODE_OF_CONDUCT.md`, `# Code of Conduct\n\n`);
+
+        rl.close();
+      });
+    });
+  });
+});
