3bcebf5f5c37c5ef42730824cb9f588396868145
Joff Tiquez <jofftiquez@gmail.com>
2023-08-01 17:20:39 +0800

    aider: Updated `rollup.config.js` to compensate for the moving of `index.js` to `src/index.js`.
    # Aider chat conversation:
    
    USER: update `rollup.config.js` to compensate the moving of index.js to src/index.js
    
    ASSISTANT: To update the `rollup.config.js` file to reflect the new location of `index.js` in the `src` directory, we need to change the `input` property in the `module.exports` object.
    
    Here is the change:
    
    ```javascript
    rollup.config.js
    <<<<<<< ORIGINAL
    module.exports = {
      input: path.resolve(__dirname, 'index.js'),
      output: {
        file: path.resolve(__dirname, 'dist/bundle.js'),
        format: 'cjs',
        exports: 'auto', // Set output.exports to "auto" to handle CommonJS exports properly
      },
      plugins: [
        resolve({
          preferBuiltins: true
        }),
        commonjs()
      ]
    };
    =======
    module.exports = {
      input: path.resolve(__dirname, 'src', 'index.js'),
      output: {
        file: path.resolve(__dirname, 'dist', 'bundle.js'),
        format: 'cjs',
        exports: 'auto', // Set output.exports to "auto" to handle CommonJS exports properly
      },
      plugins: [
        resolve({
          preferBuiltins: true
        }),
        commonjs()
      ]
    };
    >>>>>>> UPDATED
    ```
    
    This change updates the `input` property to point to the new location of `index.js` in the `src` directory.

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,9 @@ const resolve = require('rollup-plugin-node-resolve');
 const commonjs = require('rollup-plugin-commonjs');
 
 module.exports = {
-  input: path.resolve(__dirname, 'index.js'),
+  input: path.resolve(__dirname, 'src', 'index.js'),
   output: {
-    file: path.resolve(__dirname, 'dist/bundle.js'),
+    file: path.resolve(__dirname, 'dist', 'bundle.js'),
     format: 'cjs',
     exports: 'auto', // Set output.exports to "auto" to handle CommonJS exports properly
   },
