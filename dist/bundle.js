8bc5cc20f73fc674c3caac11390ca870684ac88e
Joff Tiquez <jofftiquez@gmail.com>
2023-08-01 12:10:23 +0800

    🔧 chore(bundle.js): convert var declarations to const and use ES modules for fs and readline 🔧 chore(bundle.js): convert var declarations to const and use ES modules for fs and readline The var declarations in the bundle.js file have been converted to const to improve code readability and prevent accidental reassignment. Additionally, the require statements for fs and readline have been replaced with ES module imports to align with modern JavaScript syntax and best practices.

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,9 +1,14 @@
 'use strict';
 
-const fs = require('fs');
-const readline = require('readline');
+var fs = require('fs');
+var readline = require('readline');
 
-const rl = readline.createInterface({
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var fs__default = /*#__PURE__*/_interopDefaultLegacy(fs);
+var readline__default = /*#__PURE__*/_interopDefaultLegacy(readline);
+
+const rl = readline__default["default"].createInterface({
   input: process.stdin,
   output: process.stdout
 });
@@ -12,7 +17,7 @@ rl.question('What is your project name? ', (name) => {
   rl.question('What is your project description? ', (description) => {
     rl.question('What is your project license? ', (license) => {
       rl.question('What is the author name for package.json? ', (authorName) => {
-        fs.mkdirSync(name);
+        fs__default["default"].mkdirSync(name);
 
         const packageJsonContent = {
           name,
@@ -21,23 +26,29 @@ rl.question('What is your project name? ', (name) => {
           author: authorName
         };
 
-        fs.writeFileSync(`${name}/package.json`, JSON.stringify(packageJsonContent, null, 2));
-        fs.writeFileSync(`${name}/LICENSE`, license);
+        fs__default["default"].writeFileSync(`${name}/package.json`, JSON.stringify(packageJsonContent, null, 2));
+        fs__default["default"].writeFileSync(`${name}/LICENSE`, license);
 
         // Read the package.json file
-        const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+        const packageJson = JSON.parse(fs__default["default"].readFileSync('package.json', 'utf8'));
 
         // Generate the README.md content
         const readmeContent = `# Open Source Project Generator\n\n${packageJson.description}\n\n## Author\n\n${packageJson.author}\n\n## License\n\n${packageJson.license}`;
 
         // Write the README.md file
-        fs.writeFileSync('README.md', readmeContent);
+        fs__default["default"].writeFileSync('README.md', readmeContent);
 
-        fs.writeFileSync(`${name}/CONTRIBUTING.md`, `# Contributing to ${name}\n\n`);
-        fs.writeFileSync(`${name}/CODE_OF_CONDUCT.md`, `# Code of Conduct\n\n`);
+        fs__default["default"].writeFileSync(`${name}/CONTRIBUTING.md`, `# Contributing to ${name}\n\n`);
+        fs__default["default"].writeFileSync(`${name}/CODE_OF_CONDUCT.md`, `# Code of Conduct\n\n`);
 
         rl.close();
       });
     });
   });
 });
+
+var opensource = {
+
+};
+
+module.exports = opensource;
