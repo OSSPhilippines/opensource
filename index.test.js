13d4a7809da4d20ebd419950480ee2dda73b6d6d
Joff Tiquez <jofftiquez@gmail.com>
2023-08-01 11:54:42 +0800

    Mock readline module and add tests for fs.writeFileSync.
    # Aider chat conversation:

--- index.test.js
+++ index.test.js
@@ -2,12 +2,34 @@ const fs = require('fs');
 const path = require('path');
 jest.mock('fs');
 
+// Mock readline module
+const readline = require('readline');
+jest.mock('readline');
+
 describe('index.js tests', () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
 
   test('should write README.md with correct content', () => {
+    // Mock readline.createInterface
+    readline.createInterface.mockReturnValue({
+      question: (question, callback) => {
+        // Simulate user input for each question
+        if (question === 'What is your project name? ') {
+          callback('package name');
+        } else if (question === 'What is your project description? ') {
+          callback('package description');
+        } else if (question === 'What is your project license? ') {
+          callback('package license');
+        } else if (question === 'What is the author name for package.json? ') {
+          callback('package author');
+        }
+      },
+      close: jest.fn(),
+    });
+
+    // Mock fs.readFileSync to return the packageJson content
     const packageJson = {
       name: 'package name',
       description: 'package description',
@@ -15,7 +37,14 @@ describe('index.js tests', () => {
       license: 'package license',
     };
     fs.readFileSync.mockReturnValue(JSON.stringify(packageJson));
+
+    // Run the code that uses readline and fs
     require('./index.js');
-    expect(fs.writeFileSync).toHaveBeenCalledWith('README.md', `# ${packageJson.name}\n\n${packageJson.description}\n\n## Author\n\n${packageJson.author}\n\n## License\n\n${packageJson.license}`);
+
+    // Test that fs.writeFileSync was called with the correct content
+    expect(fs.writeFileSync).toHaveBeenCalledWith(
+      'README.md',
+      `# ${packageJson.name}\n\n${packageJson.description}\n\n## Author\n\n${packageJson.author}\n\n## License\n\n${packageJson.license}`
+    );
   });
 });
