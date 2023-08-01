a2dc2bfe55ed5c3d2bdc127eb1367a87aae602a4
Joff Tiquez <jofftiquez@gmail.com>
2023-08-01 17:14:55 +0800

    Refactored the code to use the `commander` library for command line argument parsing.
    # Aider chat conversation:

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const fs = require('fs');
 const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const { program } = require('commander');
 
 const licenses = [
   'MIT',
@@ -15,42 +11,53 @@ const licenses = [
   'Unlicense'
 ];
 
-rl.question('What is your project name? ', (name) => {
-  rl.question('What is your project description? ', (description) => {
-    rl.question(`Choose a license for your project (${licenses.join(', ')}): `, (selectedLicense) => {
-      if (!licenses.includes(selectedLicense)) {
-        console.error('Invalid license selection. Please choose from the provided options.');
-        rl.close();
-        return;
-      }
+program
+  .version('1.0.0')
+  .description('Open Source Project Generator')
+  .arguments('<projectName>')
+  .action((projectName) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
 
-      rl.question('What is the author name for package.json? ', (authorName) => {
-        fs.mkdirSync(name);
+    rl.question('What is your project description? ', (description) => {
+      rl.question(`Choose a license for your project (${licenses.join(', ')}): `, (selectedLicense) => {
+        if (!licenses.includes(selectedLicense)) {
+          console.error('Invalid license selection. Please choose from the provided options.');
+          rl.close();
+          return;
+        }
 
-        const packageJsonContent = {
-          name,
-          description,
-          license: selectedLicense,
-          author: authorName
-        };
+        rl.question('What is the author name for package.json? ', (authorName) => {
+          fs.mkdirSync(projectName);
 
-        fs.writeFileSync(`${name}/package.json`, JSON.stringify(packageJsonContent, null, 2));
-        fs.writeFileSync(`${name}/LICENSE`, selectedLicense);
+          const packageJsonContent = {
+            name: projectName,
+            description,
+            license: selectedLicense,
+            author: authorName
+          };
 
-        // Read the package.json file from the project directory
-        const packageJson = JSON.parse(fs.readFileSync(`${name}/package.json`, 'utf8'));
+          fs.writeFileSync(`${projectName}/package.json`, JSON.stringify(packageJsonContent, null, 2));
+          fs.writeFileSync(`${projectName}/LICENSE`, selectedLicense);
 
-        // Generate the README.md content
-        const readmeContent = `# Open Source Project Generator\n\n${packageJson.description}\n\n## Author\n\n${packageJson.author}\n\n## License\n\n${packageJson.license}`;
+          // Read the package.json file from the project directory
+          const packageJson = JSON.parse(fs.readFileSync(`${projectName}/package.json`, 'utf8'));
 
-        // Write the README.md file
-        fs.writeFileSync(`${name}/README.md`, readmeContent);
+          // Generate the README.md content
+          const readmeContent = `# Open Source Project Generator\n\n${packageJson.description}\n\n## Author\n\n${packageJson.author}\n\n## License\n\n${packageJson.license}`;
 
-        fs.writeFileSync(`${name}/CONTRIBUTING.md`, `# Contributing to ${name}\n\n`);
-        fs.writeFileSync(`${name}/CODE_OF_CONDUCT.md`, `# Code of Conduct\n\n`);
+          // Write the README.md file
+          fs.writeFileSync(`${projectName}/README.md`, readmeContent);
 
-        rl.close();
+          fs.writeFileSync(`${projectName}/CONTRIBUTING.md`, `# Contributing to ${projectName}\n\n`);
+          fs.writeFileSync(`${projectName}/CODE_OF_CONDUCT.md`, `# Code of Conduct\n\n`);
+
+          rl.close();
+        });
       });
     });
   });
-});
+
+program.parse(process.argv);
